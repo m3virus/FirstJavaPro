ba4e7aded9d852fa7c611a42ab9cd9a298986aca
MH.Yazdani <mohaymen@YAZDANI-PC>
2025-11-08 15:12:23 +0330

    add files

---
+++ src/main/java/myjavaproject/myquestionare/SoftwareEngineerController.java
@@ -0,0 +1,27 @@
+package myjavaproject.myquestionare;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+    @GetMapping("/getEngineer")
+    public List<SoftwareEngineer> getEngineer(){
+        return List.of(
+                new SoftwareEngineer(
+                         1,
+                         "James",
+                         "C#"
+                ),
+                new SoftwareEngineer(
+                         2,
+                         "James",
+                         "java"
+                )
+        );
+    }
+}
---
+++ src/test/java/myjavaproject/myquestionare/MyQuestionareApplicationTests.java
@@ -0,0 +1,13 @@
+package myjavaproject.myquestionare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyQuestionareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
